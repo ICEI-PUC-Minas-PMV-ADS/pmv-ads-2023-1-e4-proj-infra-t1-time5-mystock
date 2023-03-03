93a1860278e4b942f620f365906127dac1e4576d
arthur <arthur.rocha@jobconvo.com>
2023-03-03 17:51:10 -0300

    Componentização dos ícones

--- src/web/src/pages/login/index.jsx
+++ src/web/src/pages/login/index.jsx
@@ -1,36 +1,57 @@
-import {Container, ContainerText, LeftLogin, RightLogin, ContainerImg, TextField, Button, EndingText, MainText} from "./styles"
+import {
+  Container,
+  ContainerText,
+  LeftLogin,
+  RightLogin,
+  ContainerImg,
+  TextField,
+  EndingText,
+  MainText,
+  Text,
+  Input,
+  Label,
+} from "../../components/componentsForm/stylesGlobal";
+import FilledButton from "../../components/filledButton";
 
-export default function Login(){
-  return <Container>
-  <LeftLogin image="images/background.png">
-    <ContainerText>
-      <h1>Olá,</h1>
-      <h2>Bem vindo ao myStock!</h2>
-      <h4>Aqui você pode criar um quadro de tarefas e compartilhá-las!</h4>
-    </ContainerText>
-    <ContainerImg>
-      <img className="principal" src="images/login-image.png" alt="desenho de um homem de camisa azul e barba colocando um icone em um tablet"/>
-    </ContainerImg>
-     
-  </LeftLogin>
-  <RightLogin>
-    <MainText>
-      <h1>Login</h1>
-      <h4>Faça seu login para acessar o app</h4>
-    </MainText>
-    <TextField>
-      <label for="e-mail">E-mail</label>
-      <input type="text" name="e-mail" placeholder="Ex: luanasilva@email.com"></input>
-      <label for="password">Senha</label>
-      <input type="password" name="password" placeholder="*******"></input>         
-    </TextField>
-    <Button>
-      Entrar
-    </Button>
-    <EndingText>
-      <h6>Ainda não tem conta? <a href="src/pages/signUp">Cadastre-se</a>
-      </h6>
-    </EndingText>
-  </RightLogin>
-</Container>
+export default function Login() {
+  return (
+    <Container>
+      <LeftLogin image="images/background.png">
+        <ContainerText>
+          <Text>Olá,</Text>
+          <h2>Bem vindo ao myStock!</h2>
+          <h4>Aqui você pode gerenciar seu estoque de produtos!</h4>
+        </ContainerText>
+        <ContainerImg>
+          <img
+            className="principal"
+            src="images/login-image.png"
+            alt="desenho de um homem de camisa azul e barba colocando um icone em um tablet"
+          />
+        </ContainerImg>
+      </LeftLogin>
+      <RightLogin>
+        <MainText>
+          <Text>Login</Text>
+          <h4>Faça seu login para acessar o app</h4>
+        </MainText>
+        <TextField>
+          <Label>E-mail</Label>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Ex: luanasilva@email.com"
+          />
+          <Label>Senha</Label>
+          <Input type="password" name="password" placeholder="*******" />
+        </TextField>
+        <FilledButton>Entrar</FilledButton>
+        <EndingText>
+          <h4>
+            Ainda não tem conta? <a href="src/pages/signUp">Cadastre-se</a>
+          </h4>
+        </EndingText>
+      </RightLogin>
+    </Container>
+  );
 }
