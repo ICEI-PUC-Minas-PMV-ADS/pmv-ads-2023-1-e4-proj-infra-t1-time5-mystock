61f4a0acfea95033335ab2ed46c4986832327f6b
arthur <arthur.rocha@jobconvo.com>
2023-03-06 22:52:44 -0300

    Ajuste botão adicionar subcategoria

--- src/web/src/components/sideManager/index.jsx
+++ src/web/src/components/sideManager/index.jsx
@@ -1,13 +1,15 @@
 import { Container } from "./styles";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useNavigate } from "react-router";
 
 export default function SideManager(props) {
+  const navigate = useNavigate();
   return (
     <Container>
       <p>
         Total de {props.type}: {props.amount}
       </p>
-      <button>
+      <button onClick={() => navigate("register")}>
         <p>
           Nov{props.type === "produtos" ? "o" : "a"}{" "}
           {props.type.replaceAll("s", "")}
