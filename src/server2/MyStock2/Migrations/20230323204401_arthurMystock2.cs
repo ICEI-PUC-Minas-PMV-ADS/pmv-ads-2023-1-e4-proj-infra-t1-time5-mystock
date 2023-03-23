b268e1c5160de75782e236b6871957e912ef29ef
arthur <arthur.rocha@jobconvo.com>
2023-03-23 17:44:16 -0300

    Ajuste connection string

---
+++ src/server2/MyStock2/Migrations/20230323204401_arthurMystock2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyStock2.Migrations
+{
+    /// <inheritdoc />
+    public partial class arthurMystock2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
